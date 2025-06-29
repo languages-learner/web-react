05a12e594fec70a7af47dddd984c6a786b42f75e
Aleksandr Chernigin <dev.chernigin@gmail.com>
2025-06-29 19:28:48 +0300

    feat: add bundle stats

---
+++ scripts/bundle-stats/get-bundle-stats.ts
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+import { execSync } from "child_process";
+import fs from "fs";
+import process from "process";
+
+import { BUNDLE_STATS_BASELINE_DIR, BUNDLE_STATS_DIR } from "../../project.config";
+
+const TEMP_CONTAINER_NAME = "temp-container";
+
+const args = process.argv.slice(2);
+const imageArgIndex = args.indexOf("--docker-image");
+const dockerImage =
+    imageArgIndex !== -1 && args[imageArgIndex + 1] ? args[imageArgIndex + 1] : null;
+const withBaseline = args.includes("--baseline");
+
+if (!dockerImage) {
+    console.error("Error: provide docker image name using '--docker-image [NAME]'");
+    process.exit(1);
+}
+
+console.log(`Using Docker image: ${dockerImage}`);
+
+if (fs.existsSync(BUNDLE_STATS_DIR)) {
+    console.log("Clear bundle-stats...");
+    fs.rmSync(`./${BUNDLE_STATS_DIR}`, { recursive: true, force: true });
+}
+
+if (withBaseline && fs.existsSync(BUNDLE_STATS_BASELINE_DIR)) {
+    console.log("Clear bundle-stats-baseline...");
+    fs.rmSync(`./${BUNDLE_STATS_BASELINE_DIR}`, { recursive: true, force: true });
+}
+
+// Create temp container and copy bundle-stats
+execSync(`docker create --name ${TEMP_CONTAINER_NAME} ${dockerImage}`, { stdio: "inherit" });
+
+try {
+    execSync(`docker cp ${TEMP_CONTAINER_NAME}:/app/${BUNDLE_STATS_DIR} ./${BUNDLE_STATS_DIR}`, {
+        stdio: "inherit",
+    });
+    if (withBaseline) {
+        execSync(
+            `docker cp ${TEMP_CONTAINER_NAME}:/app/${BUNDLE_STATS_BASELINE_DIR} ./${BUNDLE_STATS_BASELINE_DIR}`,
+            {
+                stdio: "inherit",
+            },
+        );
+    }
+} catch (e) {
+    console.log(`Copy bundle-stats error: ${e}`);
+}
+
+// Delete temp container
+execSync(`docker rm ${TEMP_CONTAINER_NAME}`, { stdio: "inherit" });
