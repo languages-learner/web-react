3eb4739e0ab8a0e77f7321f8b0734aa6b3ddd904
Aleksandr Chernigin <dev.chernigin@gmail.com>
2025-07-03 16:26:30 +0300

    fix

--- src/shared/services/api/types.ts
+++ src/shared/services/api/types.ts
@@ -10,14 +10,3 @@ export interface PaginatedResponse<T = string> {
 }
 
 export type ApiError = PostgrestError;
-
-export type {
-    Database as ApiDatabase,
-    Tables as ApiTables,
-    TablesInsert as ApiTablesInsert,
-    TablesUpdate as ApiTablesUpdate,
-    Enums as ApiEnums,
-    CompositeTypes as ApiCompositeType,
-} from "./database.types";
-
-export { Constants as ApiConstants } from "./database.types";
