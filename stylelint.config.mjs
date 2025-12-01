844d5048ee54c8306ab77e82287e858375f8fa6b
Aleksandr Chernigin <dev.chernigin@gmail.com>
2025-12-01 22:37:56 +0300

    feat: multithread eslint and updated stylelint (#20)

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,7 +1,10 @@
+/** @type {import('stylelint').Config} */
 export default {
-    extends: ["stylelint-config-recommended-scss", "stylelint-config-rational-order"],
-    plugins: ["stylelint-order", "stylelint-scss"],
-
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-standard-scss",
+        "stylelint-config-rational-order",
+    ],
     rules: {
         "selector-pseudo-class-no-unknown": [
             true,
